687b97cdb89c34028dc1e1d760ce079cd92f8cd2
Alec P <44681565+alipali737@users.noreply.github.com>
2024-02-10 13:47:13 +0000

    Delete obj directory

--- obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace LandmineScrapPlugin
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "LandmineScrapPlugin";
-        public const string PLUGIN_NAME = "LandmineScrapPlugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
